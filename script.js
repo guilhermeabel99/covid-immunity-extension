21adb69314796aab46be8bf5df57931dc042260e
guilhermeabel99 <gpeuvis@gmail.com>
2021-04-18 20:19:48 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+replaceText(document.body)
+
+function replaceText(element) {
+  if (element.hasChildNodes()) {
+    element.childNode.forEach(replaceText)
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    if (element.textContent.match(/coronavirus/gi)) {
+      const newElement = document.createElement('span')
+      newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi,
+      '<span style="background-color: black: color: black;">$1</span>')
+      element.replaceWith(newElement)
+    }
+    element.textContent = element.textContent.replace(/coronavirus/gi,
+    '  ')
+  }
+}
