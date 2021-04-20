0d181b6165a8489faca36e09b9ba3e16fc1c09bf
guilhermeabel99 <gpeuvis@gmail.com>
2021-04-19 22:47:41 -0300

    FEATURE: rainbow alert

--- script.js
+++ script.js
@@ -11,22 +11,3 @@ function replaceText(element) {
     }
   }
 }
-
-
-
-// replaceText(document.body)
-
-// function replaceText(element) {
-//   if (element.hasChildNodes()) {
-//     element.childNode.forEach(replaceText)
-//   } else if (element.nodeType === Text.TEXT_NODE) {
-//     if (element.textContent.match(/coronavirus/gi)) {
-//       const newElement = document.createElement('span')
-//       newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi,
-//       '<span style="background-color: black: color: black;">$1</span>')
-//       element.replaceWith(newElement)
-//     }
-//     element.textContent = element.textContent.replace(/coronavirus/gi,
-//     '  ')
-//   }
-// }
